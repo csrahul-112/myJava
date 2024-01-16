ff5f31cbe964773e47090db5f1fe088ada8958a3
csrahul-112 <csrahul112@gmail.com>
2024-01-16 17:53:55 +0530

    Trapping Rainwater Problem Added

---
+++ Sigma/DSA/Arrays-II/TrappingRainwater.java
@@ -0,0 +1,11 @@
+public class TrappingRainwater {
+    public static void trappingRainwater(int arr[]){
+        
+    }
+
+    public static void main(String args[]){
+        int arr[] = {4, 2, 0, 6, 3, 2, 5};
+
+        trappingRainwater(arr);
+    }
+}
