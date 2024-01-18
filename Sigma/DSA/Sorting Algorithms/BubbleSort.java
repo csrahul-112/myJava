90827544ed935167b6a699e1a294b17584271fee
csrahul-112 <csrahul112@gmail.com>
2024-01-18 16:10:57 +0530

    The bubble sort is fixed for already sorte array

--- Sigma/DSA/Sorting Algorithms/BubbleSort.java
+++ Sigma/DSA/Sorting Algorithms/BubbleSort.java
@@ -8,13 +8,22 @@ public class BubbleSort {
     public static int[] bubbleSort(int[] arr){
         int sortedArr[] = new int[arr.length];
 
+        int n = arr.length;
+        int swap = 0;
         for(int i = 0; i < (arr.length - 1); i++){
-            for(int j = 0; j < (arr.length - 1 - i); j++){
+            for(int j = 0; j < (n - 1); j++){
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swap++;
                 }
+
+            }
+            n--;//because larger elements reached their pos and need not to be sorted
+            if(swap == 0){
+                System.out.println("Already sorted");
+                break;
             }
     
         }
@@ -23,8 +32,10 @@ public class BubbleSort {
 
     public static void main(String args[]){
         int arr[] = {5, 4, 1, 3, 2};
+        int arr1[] = {1, 2, 3, 4, 5};
 
         bubbleSort(arr);
+        bubbleSort(arr1);
 
         for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
