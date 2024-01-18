b92421e2f58ac5320af82dd3c0bb74fce5702b9f
csrahul-112 <csrahul112@gmail.com>
2024-01-18 14:30:50 +0530

    The TrueIfRepeat question is done and lectures of yesterday started

---
+++ Sigma/DSA/Arrays-II/HomeWork/TrappingRainWater.java
@@ -0,0 +1,17 @@
+public class TrappingRainWater {
+    public static int waterTrapped(int[] bHeight){
+        int waterTrapped = 0;
+        //LMB
+
+        //RMB
+
+        //Calculation of water trapped
+
+        return waterTrapped;
+    }
+    public static void main(String[] args){
+        int[] buildingHeight = {1, 2, 3, 6};
+        waterTrapped(buildingHeight);
+
+    }
+}
---
+++ Sigma/DSA/Arrays-II/HomeWork/TrueIfRepeat.java
@@ -0,0 +1,24 @@
+public class TrueIfRepeat {
+
+    public static boolean trueIfRepeat(int[] arr){
+        //Solution in TC = O(n*n)
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[i] == arr[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        int arr[] = {1, 2, 3, 6, 9, 1};
+        int arr1[] = {1, 2, 3, 4, 5, 6, 7};
+        int arr2[] = {1, 1, 1, 3, 4, 5, 3, 2, 0};
+
+        System.out.println(trueIfRepeat(arr));
+        System.out.println(trueIfRepeat(arr1));
+        System.out.println(trueIfRepeat(arr2));
+    }
+}
