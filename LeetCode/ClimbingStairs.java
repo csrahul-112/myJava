fc7b3aead073c7188d5416bf785a8f0f958fe1c2
csrahul-112 <csrahul112@gmail.com>
2024-01-19 16:29:51 +0530

    #

--- LeetCode/ClimbingStairs.java
+++ LeetCode/ClimbingStairs.java
@@ -1,18 +1,4 @@
 public class ClimbingStairs {
-
-    public static int facto(int n){
-        int fact = 1;
-        if(n == 0 ){
-            return fact;
-        }else{
-            for(int i = 1; i <= n; i++){
-                fact = fact * i;
-            }
-        }
-
-        return fact;
-    }
-
     public static int climbingStairs(int n){
         int ways = 0;
 
@@ -20,7 +6,7 @@ public class ClimbingStairs {
     }
 
     public static void main(String[] args){
-        System.out.println(facto(5));
+        
 
     }
 }
