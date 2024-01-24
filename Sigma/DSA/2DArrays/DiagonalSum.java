8dad38591f3e46c13f9adb3871acacea032484eb
csrahul-112 <csrahul112@gmail.com>
2024-01-24 15:51:48 +0530

    Adding program of diagonal sum for both brute force and optimized qpproach

--- Sigma/DSA/2DArrays/DiagonalSum.java
+++ Sigma/DSA/2DArrays/DiagonalSum.java
@@ -14,12 +14,36 @@ public class DiagonalSum {
 
         return sum;
     }
+
+    public static int diagonalSumOptimized(int[][] arr){
+        int n = arr.length, sum = 0;
+
+        for(int i = 0; i < n; i++){
+            //PD
+            sum += arr[i][i];
+
+            //SD & if for the condition where n is odd and centre element is repeated
+            if(i != n - i - 1){
+                sum += arr[i][n-i-1];
+            }
+            
+        }
+        
+        
+        
+        
+        return sum;
+
+    }
     
     public static void main (String[] args) {
         int arr[][] = {{1, 2, 3},
                        {4, 5, 6},
                        {7, 8, 9}
                        };
+        
+        System.out.println(diagonalSumBF(arr));
+        System.out.println(diagonalSumOptimized(arr));
 
         
 
