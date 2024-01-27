be9e056470855be7df39dd19af93e4a52916d4bc
csrahul-112 <csrahul112@gmail.com>
2024-01-27 16:06:11 +0530

    Adding Some string Questions

---
+++ Sigma/DSA/Strings/ShortestPath.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class ShortestPath {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the String for travel : ");
+
+    
+    }
+}
