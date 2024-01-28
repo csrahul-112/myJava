c664bccc99db7fc515273a49c40c547a5c03fc62
csrahul-112 <csrahul112@gmail.com>
2024-01-28 15:07:36 +0530

    Adding program to return substring on input of starting and ending index

---
+++ Sigma/DSA/Strings/ReturningSubstring.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class ReturningSubstring {
+
+    public static String returningSubstring(String str, int si, int ei){
+        String subStr = "";
+        for(int i = si; i < ei; i++){
+            subStr += str.charAt(i);
+        }
+
+        return subStr;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = "HelloWorld";
+        System.out.println("Enter the starting index of string of length " + str.length() + " to get a Substring");
+        int si = sc.nextInt();
+        System.out.println("Enter the ending index of string of length " + str.length() + " to get a Substring");
+        int ei = sc.nextInt();
+
+        sc.close();
+
+        System.out.println(returningSubstring(str, si, ei));
+    }
+}
