225e8daefefc32123756ca2d33ff2d9a1d192128
csrahul-112 <csrahul112@gmail.com>
2024-01-29 00:28:03 +0530

    Adding StringCompression.java

---
+++ Sigma/DSA/Strings/StringCompression.java
@@ -0,0 +1,25 @@
+public class StringCompression {
+
+    public static String stringCompression(String str){
+        String str1 = "";
+
+        for(int i = 0; i < str.length(); i++){
+            Integer count = 1;
+            while((i < (str.length()-1)) && (str.charAt(i) == str.charAt(i+1))){
+                count++;
+                i++;
+            }
+            str1 += str.charAt(i);
+            if(count > 1){
+                str1 += count.toString();
+            }
+        }
+        return str1;
+    }
+    
+    public static void main(String[] args){
+        String str = "aabbccdd";
+
+        System.out.println(stringCompression(str));
+    }
+}
