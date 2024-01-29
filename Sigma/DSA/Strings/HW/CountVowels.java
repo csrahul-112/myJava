5af81d717a60477d1ef8b69e72524c30722abceb
csrahul-112 <csrahul112@gmail.com>
2024-01-29 14:00:05 +0530

    Adding a few files of String and StringBuilder

---
+++ Sigma/DSA/Strings/HW/CountVowels.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountVowels {
+
+    public static int countLowerCaseVowels(String str){
+        int count = 0;
+        for(int i = 0; i < str.length(); i++){
+            if((str.charAt(i) == 'a') || (str.charAt(i) == 'e') || (str.charAt(i) == 'i' )|| (str.charAt(i) == 'o') || (str.charAt(i) == 'u')){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string : ");
+        String str = sc.nextLine();
+
+        sc.close();
+
+        System.out.println(countLowerCaseVowels(str));
+
+    }
+}
--- Sigma/DSA/Strings/StringCompression.java
+++ Sigma/DSA/Strings/StringCompression.java
@@ -1,5 +1,6 @@
 public class StringCompression {
 
+    //with larger time complexity
     public static String stringCompression(String str){
         String str1 = "";
 
@@ -16,10 +17,30 @@ public class StringCompression {
         }
         return str1;
     }
+
+    //With smaller time complexity
+    public static String stringCompressionOptimal(String str){
+        StringBuilder sb = new StringBuilder("");
+
+        for (int i = 0; i < str.length(); i++){
+            Integer count = 1;
+            while(i < (str.length()-1) && (str.charAt(i) == str.charAt(i+1))){
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count > 1){
+                sb.append(count.toString());
+            }
+        }
+
+        return sb.toString();
+    }
     
     public static void main(String[] args){
-        String str = "aabbccdd";
+        String str = "aaaaabbccdd";
 
-        System.out.println(stringCompression(str));
+        //System.out.println(stringCompression(str));
+        System.out.println(stringCompressionOptimal(str));
     }
 }
