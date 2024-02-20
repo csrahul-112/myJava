ac2ac371779814e7bcae7188eb34ffeafd1d693e
csrahul-112 <csrahul112@gmail.com>
2024-02-21 00:25:39 +0530

    Adding some BackTracking Question

--- Sigma/Recursion/BackTracking/NQueens1Sol.java
+++ Sigma/Recursion/BackTracking/NQueens1Sol.java
@@ -1,43 +1,47 @@
-public class NQueens {
+public class NQueens1Sol {
     public static boolean isSafe(char[][] board, int row, int col){
-        //vertical up
+        //vertical up  
         for(int i = row-1; i >= 0; i--){
             if (board[i][col] == 'Q') {
                 return false;
             }
         }
+
         //left up diagonal
-        for(int i = row-1, j = col-1; i >= 0 && j >= 0; i--, j--){
+        for(int i = row -1, j = col -1; i >= 0 && j >= 0; i--, j--){
             if (board[i][j] == 'Q') {
                 return false;
             }
         }
 
         //right up diagonal
-        for(int i = row-1, j = col+1; i >= 0 && j < board.length; i--, j++){
+        for(int i = row-1, j = col + 1; i >= 0 && j < board.length; i--, j++){
             if (board[i][j] == 'Q') {
-                return false;
+                return false;  
             }
         }
 
         return true;
     }
 
-    public static void nQueens(char[][] board, int row){
+    public static boolean nQueens(char[][] board, int row){
         if(row >= board.length){
-            //printBoard(board);
-            count++;
-            return;
+            return true;
         }
 
         for(int j = 0; j < board.length; j++){
-            if (isSafe(board, row, j)) {
+            if(isSafe(board, row, j)){
                 board[row][j] = 'Q';
-                nQueens(board, row+1);
-                board[row][j] = 'X';
-            }
+                if(nQueens(board, row+1)){
+                    return true;
+                }else{
+                    board[row][j] = 'X';
+                }   
+            } 
         }
+        return false;
     }
+
     public static void printBoard(char[][] board){
         System.out.println("----------Chess Board-----------");
         for(int i = 0; i < board.length; i++){
@@ -47,17 +51,20 @@ public class NQueens {
             System.out.println();
         }
     }
-    
-    static int count = 0;
-    public static void main(String args[]){
-        int n = 8;
-        char board[][] = new char[n][n];
-        for(int i = 0; i < n; i++){
+    public static void main(String[] args){
+        int n = 4;
+        char[][] board = new char[n][n];
+        for(int i = 0; i < n; i ++){
             for(int j = 0; j < n; j++){
                 board[i][j] = 'X';
             }
         }
-        nQueens(board, 0);
-        System.out.println("Total ways to place the " + n + " Queens at nxn board is: " + count) ;
+
+        if (nQueens(board, 0)) {
+            System.out.println("Solution is Possible");
+            printBoard(board);
+        }else{
+            System.out.println("Solution is NOT Possible");
+        }
     }
 }
